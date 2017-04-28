b9370e6e2c64fffa7b644a5f751c401ce108a320
tongari <info@tongari-as.com>
2017-04-28 14:12:16 +0900

    Update

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,21 @@
-import wait from './domain/utils/wait';
 
-wait();
-console.log('app');
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!'
+  }
+});
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date()
+  }
+});
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: false
+  }
+});
